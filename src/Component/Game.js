1375b825dea4dcf6ca982d92df3aa1f9906c4806
Kang-Yujin <yjkang0830@gmail.com>
2022-04-03 10:45:33 +0900

    second commit

--- src/Component/Game.js
+++ src/Component/Game.js
@@ -78,11 +78,12 @@ export default function Game() {
     return (
         <div className="game">
             <div className="game-board">
-                <Board squares = {current} onClick = { (i) => handleClick(i) } />
+                <Board squares={current} onClick={(i) => handleClick(i)} />
+                <br /><br />Written by Yujin Kang
             </div>
             <div className="game-info">
-                <div>{ status }</div>
-                <ol>{ moves }</ol>
+                <div>{status}</div>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
